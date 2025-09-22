49e0d60e9af7a8d0c7604a6653f4d095229d6465
masoomehmokhtari78 <masoomehmokhtari693@gmail.com>
2025-09-22 20:20:16 +0330

    feat: add navigation menu

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,14 +1,23 @@
 "use client";
 
+import React, { ReactNode } from "react";
+import { useSelectedLayoutSegments } from "next/navigation";
+
+import Link from "next/link";
+import {
+  NavigationMenu,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+} from "@/components/ui/navigation";
+import Image from "next/image";
+
 const titleMap = {
   Dashboard: "تراکنش ها",
   Transactions: "تراکنش ها",
   Users: "کاربران",
   Reports: "گزارشات سیستمی",
 };
-
-import { useSelectedLayoutSegments } from "next/navigation";
-import React, { ReactNode } from "react";
 
 export default function Layout({ children }: { children: ReactNode }) {
   const segments = useSelectedLayoutSegments();
@@ -19,11 +28,39 @@ export default function Layout({ children }: { children: ReactNode }) {
     lastSegment.slice(1).replace(/-/g, " ");
 
   return (
-    <div className="w-full h-screen">
-      <div className="flex flex-col items-center justify-center p-6 gap-6 h-full min-h-fit">
+    <div className="w-full h-screen flex flex-col">
+      <header className="p-6 border-b border-gray-200 dark:border-gray-800 flex justify-between">
+        <Image src="/vercel.svg" height={40} width={40} alt="logo" />
+        <NavigationMenu>
+          <NavigationMenuList className="flex-row-reverse">
+            <NavigationMenuItem>
+              <NavigationMenuLink asChild>
+                <Link href="/dashboard">داشبورد</Link>
+              </NavigationMenuLink>
+            </NavigationMenuItem>
+            <NavigationMenuItem>
+              <NavigationMenuLink asChild>
+                <Link href="/dashboard/transactions">تراکنش ها</Link>
+              </NavigationMenuLink>
+            </NavigationMenuItem>
+            <NavigationMenuItem>
+              <NavigationMenuLink asChild>
+                <Link href="/dashboard/users">کاربران</Link>
+              </NavigationMenuLink>
+            </NavigationMenuItem>
+            <NavigationMenuItem>
+              <NavigationMenuLink asChild>
+                <Link href="/dashboard/reports">گزارشات</Link>
+              </NavigationMenuLink>
+            </NavigationMenuItem>
+          </NavigationMenuList>
+        </NavigationMenu>
+      </header>
+
+      <main className="flex-1 flex flex-col items-center justify-start p-6 gap-6">
         <h1 className="text-2xl font-bold">{titleMap[title] || "داشبورد"}</h1>
         {children}
-      </div>
+      </main>
     </div>
   );
 }
